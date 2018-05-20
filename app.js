8f8ae72355a4a1414a074478604b3eccf7e67fa5
Rhesa Utomo <venderblitz@gmail.com>
2018-05-20 16:16:26 +0700

    release 5 finished

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 const express = require('express');
 const routes = require('./routes')
 let app = express();
+let bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
 
 // Connect all our routes to the application
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,65 @@ const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
   // res.status(200).json({ message: 'Connected!' })
-  res.send('Cheers love, the cavalry\'s here!');
+  // res.send('Cheers love, the cavalry\'s here!');
+  res.render('index.ejs');
 })
 
 routes.get('/student', (req, res) => {
-  res.send('Input student data form')
+  // res.send('Input student data form')
+  res.render('student.ejs', {studentTitle: 'Input student data'})
 })
 
 routes.post('/student', (req, res) => {
-  res.send('Got a POST request');
+  // res.send('Got a POST request');
+  let firstName = req.body.firstName,
+      lastName = req.body.lastName,
+      email = req.body.email;
+
+  let student = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email 
+  };
+
+  let display = `First name: ${student.firstName}
+  Last name: ${student.lastName}
+  Email: ${student.email}`
+  // res.send(students)
+  res.render('submitted.ejs', {
+    headerTitle: 'Data result',
+    result: display
+  })
 })
 
-routes.get('/teachers', (req, res) => {
-  res.send('Display teachers data')
+routes.get('/teacher', (req, res) => {
+  // res.send('Display teachers data')
+  let teacher = [{
+    firstName: 'Tony',
+    lastName: 'Stark',
+    city: 'New York',
+    email: 'tony@avengers.com'
+  }, {
+    firstName: 'Bruce',
+    lastName: 'Banner',
+    city: 'New York',
+    email: 'bruce@avengers.com'
+  }, {
+    firstName: 'Jack',
+    lastName: 'Morisson',
+    city: 'Gibraltar',
+    email: 'jack76@overwatch.com'
+  }, {
+    firstName: 'Hanzo',
+    lastName: 'Shimada',
+    city: 'Japan',
+    email: 'ryuugawagatekiwokurau@shimada.com'
+  }]
+
+  res.render('teacher.ejs', {
+    headerTitle: 'Teacher data',
+    data: teacher
+  })
 })
 
 module.exports = routes;
